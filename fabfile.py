0e34dd6c59a51cc3227df640cfd3e9fc049077d3
guoweikuang <673411814@qq.com>
2018-07-04 14:27:56 +0800

    add docker build config

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import with_statement
-from fabric.api import local
-from fabric.api import run
-from fabric.api import settings
-from fabric.api import cd
+from fabric.api import *
+
+
+env.user = 'ubuntu'
+env.hosts = ['119.29.172.148']
+env.key_filename = "/home/guoweikuang/project/my/daocloud"
 
 
 def prepare_deploy():
@@ -11,7 +13,7 @@ def prepare_deploy():
     local('git push origin develop')
 
 
-def deploy(project_link):
+def deploy():
     """ deploy project to remote.
 
     :return:
@@ -21,5 +23,6 @@ def deploy(project_link):
         if run('test -d %s' % project_dir).failed:
             run("git clone https://github.com/guoweikuang/flask_v2ex.git")
     with cd(project_dir):
+        run("git clone https://github.com/guoweikuang/flask_v2ex.git")
         run('git pull')
 
